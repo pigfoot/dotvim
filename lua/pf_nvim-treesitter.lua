3a0b02bdd5212478c9ce201dd81e54b4619c534c
Chen, Chih-Chia <pigfoot@gmail.com>
2022-11-24 19:15:17 +0800

    [nvim-treesitter] error handling
    Signed-off-by: Chen, Chih-Chia <pigfoot@gmail.com>

--- lua/pf_nvim-treesitter.lua
+++ lua/pf_nvim-treesitter.lua
@@ -18,6 +18,8 @@ require('nvim-treesitter.highlight')
 local hlmap = vim.treesitter.highlighter.hl_map
 
 --Misc
-hlmap.error = nil
-hlmap["punctuation.delimiter"] = "Delimiter"
-hlmap["punctuation.bracket"] = nil
+if (nlmap ~= nil) then
+  hlmap.error = nil
+  hlmap["punctuation.delimiter"] = "Delimiter"
+  hlmap["punctuation.bracket"] = nil
+end
